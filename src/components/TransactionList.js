25bbff9951f2180baa6466101354a722489381ce
GeoHernandez27 <leopardo2@gmail.com>
2020-02-28 11:09:35 -0700

    updated history sign components

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,5 +1,9 @@
 import React, { useContext} from "react";
-import { GlobalContext } from '../context/GlobalState'
+import { Transaction } from './Transaction';
+
+import { GlobalContext } from '../context/GlobalState';
+
+
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
@@ -11,10 +15,7 @@ export const TransactionList = () => {
     <>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(trasnaction => (        <li className="minus">
-          {transactions.text} <span>-$400</span>
-          <button className="delete-btn">X</button>
-        </li>))}
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
 
       </ul>
     </>
